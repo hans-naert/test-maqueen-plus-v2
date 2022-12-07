c993190d2e9dab91d560421961ebbef93da774f0
VIVES-ELICT <hans.naert@vives.be>
2022-12-07 17:39:11 +0100

    first version

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+DFRobotMaqueenPlusV2.I2CInit()
+basic.showString(DFRobotMaqueenPlusV2.readVersion())
+serial.writeLine(DFRobotMaqueenPlusV2.readVersion())
 basic.forever(function () {
-    basic.showString("Hello!")
+    if (DFRobotMaqueenPlusV2.readLineSensorState(MyEnumLineSensor.eL1) == 1) {
+        DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+    } else {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 100)
+    }
 })
